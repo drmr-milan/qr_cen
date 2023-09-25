28e986f6a37ac76202567e9f17c88f6b69435b89
drmr-milan <missing.bloke@gmail.com>
2023-09-25 15:07:28 +0200

    rework category_order

--- components/Category_order.jsx
+++ components/Category_order.jsx
@@ -3,20 +3,16 @@
 import { useForm } from "react-hook-form";
 import { mutate } from "swr";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Order_cat_schema } from "@/utils/ValidationShemas";
-
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Form } from "@/components/ui/form";
 
 import { IconArrowBadgeUp } from "@tabler/icons-react";
 
 const fecher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Category_order({ local_id, cat_type, items_type, cat_id, order_num }) {
-	const form = useForm({ resolver: zodResolver(Order_cat_schema) });
+	const form = useForm();
 	const { toast } = useToast();
 
 	async function onSubmit(data) {
@@ -27,7 +23,7 @@ export default function Category_order({ local_id, cat_type, items_type, cat_id,
 
 		const send_data = await fecher(`/api/admin/${local_id}/categorys/${cat_type}/${items_type}`, {
 			method: "PATCH",
-			body: JSON.stringify({ order_num: data.order_num }),
+			body: JSON.stringify({ order_num }),
 		}).catch((error) => {
 			console.log(error);
 			alert("Došlo je do greške.");
@@ -49,20 +45,7 @@ export default function Category_order({ local_id, cat_type, items_type, cat_id,
 					onSubmit={form.handleSubmit(onSubmit)}
 					id={`order_${cat_id}_form`}
 					className="hidden"
-				>
-					<FormField
-						control={form.control}
-						name="order_num"
-						defaultValue={order_num}
-						render={({ field }) => (
-							<FormItem>
-								<FormControl>
-									<Input {...field} />
-								</FormControl>
-							</FormItem>
-						)}
-					/>
-				</form>
+				></form>
 			</Form>
 			<Button
 				type="submit"
